7047cb933f640caa32d66b255b24af6a0c9688c6
Oussama Moussaoui <oussamamoussaoui.business@gmail.com>
2021-09-02 19:50:20 +0100

    made 2d peak finder

--- ex00/find_peak.c
+++ ex00/find_peak.c
@@ -6,24 +6,32 @@
 /*   By: os-moussao <omoussaoui040@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 23:01:17 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/09/01 23:09:55 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/09/02 13:38:18 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * find peak in 1d array
+*/
+
 #include <stdio.h>
 
-int	find_peak(int *tab, int start, int end, int len)
+int	find_peak(int *tab, int start, int end)
 {
-	if (tab[start + len/2] < tab[start + len/2 + 1])
-		return (find_peak(tab, start + len/2 + 1, end, len / 2));
-	else if ((tab[start + len/2] < tab[start + len/2 - 1]))
-		return (find_peak(tab, start, start + len/2 - 1, len / 2));
-	else
-		return (len/2);
+	int	mid;
+
+	mid = (end + start) / 2;
+	if (start == end)
+		return (start);
+	if (tab[mid] < tab[mid + 1])
+		return (find_peak(tab, mid + 1, end));
+	if (tab[mid] < tab[mid - 1])
+		return (find_peak(tab, start, mid - 1));
+	return (mid);
 }
 
 int	main(void)
 {
-	int	tab[10] = {0,1,2,3,4,5,6,7,8,9};
-	printf("%d", tab[find_peak(tab, 0, 9, 10)]);
+	int	tab[] = {11, 10, 9, 8, 9, 6, 5};
+	printf("%d", tab[find_peak(tab, 0, 6)]);
 }
