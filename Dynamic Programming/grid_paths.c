c7a3c48a5cc99c51f25bff6265f157f82790b6ee
Oussama Moussaoui <oussamamoussaoui.business@gmail.com>
2021-09-09 13:09:55 +0100

    Update: added another solution to grid_paths

--- Dynamic Programming/grid_paths.c
+++ Dynamic Programming/grid_paths.c
@@ -6,12 +6,14 @@
 /*   By: os-moussao <omoussaoui040@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 23:10:55 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/09/09 11:01:38 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/09/09 13:08:11 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+// Time: m*n
+// Space: m*n
 int	grid_paths(int n, int m)
 {
 	int	arr[n][m];
@@ -27,6 +29,31 @@ int	grid_paths(int n, int m)
 		}
 	}
 	return (arr[n - 1][m - 1]);
+}
+
+
+// Another dynamic programming solution: using only one dimensional array of size max(m, n)
+// Space Complexity of max(n, m) rather than m*n (grid_paths)
+// Time complexity: m*n
+int uniquePaths(int n, int m)
+{
+	int	max = (n >= m)? n: m;
+	int	min = (max == n)? m: n;
+	int	arr[max];
+    
+	for (int i = 0; i < max; i++)
+		arr[i] = 1;
+	for (int i = 1; i < min; i++)
+	{
+		for (int j = i; j < max; j++)
+		{
+			if (j == i)
+				arr[j] *= 2;
+			else
+				arr[j] += arr[j - 1];
+		}
+	}
+	return (arr[max - 1]);
 }
 
 int	main(void)
