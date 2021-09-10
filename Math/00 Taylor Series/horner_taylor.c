99db7eebf379e606c864bee473df47618b800737
Oussama Moussaoui <oussamamoussaoui.business@gmail.com>
2021-09-11 00:01:57 +0100

    Solved: horner_taylor (iterative)

--- Math/00 Taylor Series/horner_taylor.c
+++ Math/00 Taylor Series/horner_taylor.c
@@ -6,7 +6,7 @@
 /*   By: os-moussao <omoussaoui040@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 22:38:22 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/09/09 23:30:32 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/09/11 00:00:41 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ double	taylor(double x, int n)
 
 	for (double i = n; i > 0; i--)
 		res = res * x / i + 1;
+	return (res);
 }
 
 int	main(void)
 {
-	printf("%lf", taylor(3, 15));
+	printf("%lf", taylor(3, 50));
 }
