8d7348cbd9ef792c8ee20aaa744c9fb6028a0afb
Oussama Moussaoui <oussamamoussaoui.business@gmail.com>
2021-09-13 12:26:04 +0100

    Solved: Sudoku Solver

--- Backtracking/02 Sudoku Solver/sudoku_solver.c
+++ Backtracking/02 Sudoku Solver/sudoku_solver.c
@@ -6,29 +6,14 @@
 /*   By: os-moussao <omoussaoui040@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 17:28:00 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/09/12 23:11:34 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/09/13 12:25:00 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-
-int		can_ch(int row, int col)
-{
-	char	board[9][9] = 
-	{
-		{'5','3','.','.','7','.','.','.','.'},
-		{'6','.','.','1','9','5','.','.','.'},
-		{'.','9','8','.','.','.','.','6','.'},
-		{'8','.','.','.','6','.','.','.','3'},
-		{'4','.','.','8','.','3','.','.','1'},
-		{'7','.','.','.','2','.','.','.','6'},
-		{'.','6','.','.','.','.','2','8','.'},
-		{'.','.','.','4','1','9','.','.','5'},
-		{'.','.','.','.','8','.','.','7','9'}
-	};
-
-	return (board[row][col] == '.');
-}
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int     valid_choice(char bd[][9], int row, int col, char choice)
 {
@@ -64,10 +49,89 @@ int     valid_choice(char bd[][9], int row, int col, char choice)
     return (1);
 }
 
+int	next_pos(char board[][9], int *row, int *col)
+{
+	for (; (*row) < 9; (*row)++)
+	{
+		for (; (*col) < 9; (*col)++)
+		{
+			if (board[*row][*col] == '.')
+				return (0);
+		}
+		(*col) = 0;
+	}
+	return (1);
+}
+
+bool	solve_sudoku(char board[][9], int row, int col)
+{
+	int solved = next_pos(board, &row, &col);
+
+	if (solved)
+		return (true);
+
+	for (char choice = '1'; choice <= '9'; choice++)
+	{
+        if (valid_choice(board, row, col, choice))
+		{
+            board[row][col] = choice;
+
+			// Backtracking
+			if (solve_sudoku(board, row, col))
+				return (true);
+
+			board[row][col] = '.';
+		}
+	}
+
+	return (false);
+}
+
+void	put_board(char board[][9])
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (i && i % 3 == 0)
+			printf("\n");
+		for (int j = 0; j < 9; j++)
+		{
+			if (j && j % 3 == 0)
+				printf(" ");
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int	main(void)
+{
+	char	puzzle[9][9];
+
+	scanf(" %[^\n]%*c", puzzle[0]);
+	scanf(" %[^\n]%*c", puzzle[1]);
+	scanf(" %[^\n]%*c", puzzle[2]);
+	scanf(" %[^\n]%*c", puzzle[3]);
+	scanf(" %[^\n]%*c", puzzle[4]);
+	scanf(" %[^\n]%*c", puzzle[5]);
+	scanf(" %[^\n]%*c", puzzle[6]);
+	scanf(" %[^\n]%*c", puzzle[7]);
+	scanf(" %[^\n]%*c", puzzle[8]);
+
+	printf("\nBefore:\n");
+	put_board(puzzle);
+	
+	solve_sudoku(puzzle, 0, 0);
+
+	printf("\n\nResult:\n");
+	put_board(puzzle);
+}
+
+/*
+
+How I tried to solve this problem before:
+
 void    backtrack(char board[][9], int row, int col, int can_change)
 {
-	//int	val;
-
 	if (row == 9)
 		return ;
 
@@ -79,18 +143,6 @@ void    backtrack(char board[][9], int row, int col, int can_change)
 			backtrack(board, row, col + 1, 0);
 	}
 
-	//val = can_ch(row, col);
-	/*
-	if (!can_change)
-	{
-		if (col == 0)
-			printf("bd[%d][%d]: %c\n", row - 1, 8, board[row - 1][8]);
-		else
-			printf("bd[%d][%d]: %c\n", row, col - 1, board[row][col - 1]);
-
-	}
-	*/
-	
     if (board[row][col] == '.')
     {
         for (char choice = '1'; choice <= '9'; choice++)
@@ -123,41 +175,4 @@ void    backtrack(char board[][9], int row, int col, int can_change)
 	else
 		backtrack(board, row, col + 1, 1);
 }
-
-void	put_board(char board[][9])
-{
-	for (int i = 0; i < 9; i++)
-	{
-		if (i && i % 3 == 0)
-			printf("\n");
-		for (int j = 0; j < 9; j++)
-		{
-			if (j && j % 3 == 0)
-				printf(" ");
-			printf("%c ", board[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-int	main(void)
-{
-	char	board[9][9] = 
-	{
-		{'5','3','.','.','7','.','.','.','.'},
-		{'6','.','.','1','9','5','.','.','.'},
-		{'.','9','8','.','.','.','.','6','.'},
-		{'8','.','.','.','6','.','.','.','3'},
-		{'4','.','.','8','.','3','.','.','1'},
-		{'7','.','.','.','2','.','.','.','6'},
-		{'.','6','.','.','.','.','2','8','.'},
-		{'.','.','.','4','1','9','.','.','5'},
-		{'.','.','.','.','8','.','.','7','9'}
-	};
-
-	put_board(board);
-
-	backtrack(board, 0, 0, 1);
-	printf("\nResult:\n");
-	put_board(board);
-}
+*/
