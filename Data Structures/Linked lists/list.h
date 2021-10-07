e1c9dc7c2df18d9a5b13534f26d55ba7dd863b42
Oussama Moussaoui <oussamamoussaoui.business@gmail.com>
2021-10-07 20:29:47 +0100

    Update: added header files to list.h

--- Data Structures/Linked lists/list.h
+++ Data Structures/Linked lists/list.h
@@ -6,12 +6,15 @@
 /*   By: os-moussao <omoussao@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 11:17:12 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/10/07 11:47:04 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/10/07 20:28:12 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIST_H
 #define LIST_H
+
+# include <stdio.h>
+# include <stdlib.h>
 
 typedef struct	s_node
 {
