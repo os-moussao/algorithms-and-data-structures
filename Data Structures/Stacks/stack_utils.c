adf082fc27ecaa79845666d162a1a35759cc884e
os-moussao <omoussaoui1337@gmail.com>
2021-10-29 23:44:28 +0100

    added alternative process for memcpy

--- Data Structures/Stacks/stack_utils.c
+++ Data Structures/Stacks/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: os-moussao <omoussao@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 21:24:37 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/10/29 23:29:25 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/10/29 23:42:56 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 void	stack_push(t_node **stack, void *data, size_t data_size)
 {
 	t_node	*new = (t_node *)malloc(sizeof(t_node));
-
 	new->data = malloc(data_size);
-	new->next = (*stack);
-
-	//for (int i = 0; i < data_size; i++)
-	//	*(char *)(new->data + i) = *(char *)(data + i);
 	memcpy(new->data, data, data_size);
-
+	new->next = (*stack);
 	(*stack) = new;
 }
+/* alternative process for memcpy */
+// for (int i = 0; i < data_size; i++)
+// 	*(char *)(new->data + i) = *(char *)(data + i);
 
 void	stack_pop(t_node **stack)
 {
