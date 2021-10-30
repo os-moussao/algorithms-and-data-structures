3e21e91aa65a9af42612a6497298c450a74751f2
os-moussao <omoussaoui1337@gmail.com>
2021-10-30 13:17:24 +0100

    added infix to postfix conversion

--- Data Structures/Stacks/01 infix to postfix/infix_to_postfix.c
+++ Data Structures/Stacks/01 infix to postfix/infix_to_postfix.c
@@ -6,19 +6,76 @@
 /*   By: os-moussao <omoussao@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 20:48:21 by os-moussao        #+#    #+#             */
-/*   Updated: 2021/10/29 23:30:30 by os-moussao       ###   ########.fr       */
+/*   Updated: 2021/10/30 13:16:24 by os-moussao       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../stack.h"
 #include "../stack_utils.c"
 
-int	main(void)
+int	precedence(char op)
+{
+	if (op == '^')
+		return (3);
+	else if (op == '*' || op == '/')
+		return (2);
+	else
+		return (1);
+}
+
+bool	is_op(char c)
+{
+	return (c == '^' || c == '*' || c == '/' || c == '+' || c == '-');
+}
+
+/**
+ * todo: parentheses handling
+ **/
+char	*convert(char *exp)
 {
 	t_node	*stack = NULL;
-	int		n = 1337;
+	char	*postfix = malloc(strlen(exp));
+	int		i = 0;
 
-	stack_push(&stack, &n, sizeof(n));
-	printf("%d\n", *(int *)stack->data);
-	stack_clear(&stack);
+	while (*exp)
+	{
+		if (*exp != ' ')
+		{
+			if (!is_op(*exp))
+				postfix[i++] = *exp;
+			else
+			{
+				if (!stack || precedence(*exp) > precedence(*(char *)(stack->data)))
+					stack_push(&stack, exp, 1);
+				else
+				{
+					while (stack && precedence(*exp) <= precedence(*(char *)(stack->data)))
+						postfix[i++] = *(char *)(stack->data),
+						stack_pop(&stack);
+					stack_push(&stack, exp, 1);
+				}
+			}
+		}
+		exp++;
+	}
+	while (stack)
+	{
+		postfix[i++] = *(char *)(stack->data);
+		stack_pop(&stack);
+	}
+	postfix[i] = '\0';
+	return (postfix);
+}
+
+int	main(void)
+{
+	char	expression[250];
+	char	*postfix;
+
+	printf("Enter an infix expression: ");
+	scanf(" %[^\n]", expression);
+
+	postfix = convert(expression);
+	printf("expression to postfix: %s\n", postfix);
+	free(postfix);
 }
