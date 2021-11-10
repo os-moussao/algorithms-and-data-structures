ff22886445dd2e6b7d2e8f82fe0c7a3bfd65aed5
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-10 12:54:03 +0100

    added sqrt

---
+++ Algorithms/Searching Algorithms/Binary Search/00 first step/sqrt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double	os_sqrt(double x)
+{
+	if (x < 0)
+		return (-1);
+	double	mid, l = 0, h = x, eps = 0.000001;
+	while (h - l >= eps)
+	{
+		mid = l + (h - l) / 2;
+		if (mid*mid < x)
+			l = mid;
+		else
+			h = mid;
+	}
+	return (mid);
+}
+
+int	main(void)
+{
+	printf("%lf", os_sqrt(2));
+}
