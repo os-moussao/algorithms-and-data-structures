6d7fa074c13e940f94a6513d26a6d222525ad573
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-10 12:57:02 +0100

    updated sqrt

--- Algorithms/Searching Algorithms/Binary Search/00 first step/sqrt.c
+++ Algorithms/Searching Algorithms/Binary Search/00 first step/sqrt.c
@@ -5,7 +5,7 @@ double	os_sqrt(double x)
 	if (x < 0)
 		return (-1);
 	double	mid, l = 0, h = x, eps = 0.000001;
-	while (h - l >= eps)
+	while (h - l > eps)
 	{
 		mid = l + (h - l) / 2;
 		if (mid*mid < x)
