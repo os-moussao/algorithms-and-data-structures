e2803f2410dc446f5f06aa32eb050064a9dfd847
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-22 12:08:03 +0100

    added random mood solution

--- Competitive Programming/Matrix Exponentiation/random_mood.cpp
+++ Competitive Programming/Matrix Exponentiation/random_mood.cpp
@@ -1,3 +1,4 @@
+// https://codeforces.com/gym/102644/problem/A
 #include <iostream>
 #include <iomanip>
 
@@ -8,51 +9,52 @@ using namespace std;
 #define nn '\n'
 #define ss ' '
 
-double solve_err(int n, double p)
+double solve(int n, double p);
+double solve_me(int n, double p);
+
+int32_t main()
+{
+	int n;
+	double p;
+	cin >> n >> p;
+
+	cout << fixed << setprecision(10) << solve(n, p) << nn;
+	return 0;
+}
+
+/* errichto's way in O(log(n)) */
+double solve(int n, double p)
 {
 	double ans = 1.0;
 
 	while (n > 0)
 	{
 		if (n % 2 == 1)
-		{
 			ans = ans * (1 - p) + (1 - ans) * p;
-		}
-		p *= 2 * (1 - p);
+		p = 2 * p * (1 - p);
 		n /= 2;
 	}
 	return (ans);
 }
 
+/* how I tried to solve this problem, in O(n)
+ * time limit exceeded in test 30
+ **/
 double solve_me(int n, double p)
 {
-	double ans = 1.0;
+	double happy = 1.000, sad = 0.000;
 
 	if (n % 2 == 1)
 	{
-		ans *= (1 - p);
-		if (--n)
-		{
-			ans = ans * ((1 - p)*(1 - p) + p*p) + 2 * p * (1 - p) * (1 - ans);
-			n /= 2;
-		}
+		happy *= (1 - p);
+		sad = 1 - happy;
+		n--;
 	}
 	while (n > 1)
 	{
-		ans *= ((1 - p)*(1 - p) + p*p);
-		n /= 2;
+		happy = happy * ((1-p)*(1-p) + p*p) + sad * 2 * (1-p) * p;
+		sad = 1 - happy;
+		n -= 2;
 	}
-	return (ans);
-}
-
-int32_t main()
-{
-	cout << setprecision(10);
-	int n;
-	double p;
-	cin >> n >> p;
-
-	cout << fixed << setprecision(10) << solve_err(n, p) << nn;
-	cout << fixed << setprecision(10) << solve_me(n, p) << nn;
-	return 0;
+	return (happy);
 }
