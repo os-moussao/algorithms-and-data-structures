d5774e0f82a9c3cb9fdadc12cb38054d2e038309
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-29 23:21:09 +0100

    solved prA

--- Competitive Programming/Practice/Codeforces/div. 2/02 deltix rd/divMult.cpp
+++ Competitive Programming/Practice/Codeforces/div. 2/02 deltix rd/divMult.cpp
@@ -14,38 +14,6 @@ using namespace std;
 #define nn '\n'
 #define ss ' '
 
-const int MOD = 1e9 + 7;
-
-int minE(int *arr, int n)
-{
-	int min = LLONG_MAX, in = -1;
-
-	for (int i = 0; i < n; i++)
-	{
-		if (!(arr[i]&1) && arr[i] <= min)
-		{
-			min = arr[i];
-			in = i;
-		}
-	}
-	return (in);
-}
-
-int maxE(int *arr, int n)
-{
-	int max = LLONG_MIN, in = -1;
-
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)
-		{
-			max = arr[i];
-			in = i;
-		}
-	}
-	return (in);
-}
-
 int maxO(int *arr, int n)
 {
 	int max = LLONG_MIN, in = -1;
@@ -61,7 +29,6 @@ int maxO(int *arr, int n)
 	return (in);
 }
 
-
 void solve()
 {
 	int n;
@@ -69,34 +36,26 @@ void solve()
 
 	int arr[n];
 	REP(i, n) cin >> arr[i];
-
-	int ma, mi;
-	ma = maxE(arr, n);
-	mi = minE(arr, n);
-	while (1)
-	{
-
-		cout << nn;
-		REP(i, n) cout << arr[i] << ss;
-		cout << nn;
-
-		if (mi == -1)
-			break ;
-		if (ma == mi)
-		{
-			ma = maxO(arr, n);
-			if (arr[ma] == 1)
-				break ;
-		}
-		arr[mi] >>= 1;
-		arr[ma] <<= 1;
-		ma = maxE(arr, n);
-		mi = minE(arr, n);
-	}
-
+	
+	int k = 1;
 	int s = 0;
-	REP(i, n) s += arr[i];
-	cout << s << nn;
+	int m = -1;
+	int in = -1;
+	REP(i, n)
+	{
+		while (!(arr[i]&1))
+		{
+			arr[i] >>= 1;
+			k <<= 1;
+		}
+		if (arr[i] > m)
+		{
+			m = arr[i];
+			in = i;
+		}
+		s += arr[i];
+	}
+	cout << s - arr[in] + arr[in] * k << nn;
 }
 
 int32_t main()
