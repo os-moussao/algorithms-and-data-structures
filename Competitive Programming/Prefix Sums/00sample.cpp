512ed2ee2a537281db79a6b5c1100332f03ae254
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-02 18:57:44 +0100

    started prefix sums

---
+++ Competitive Programming/Prefix Sums/00sample.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define int long long
+#define double long double
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+#define REP(i, a, n) for(int i = a; i < n; i++)
+#define SZ size()
+#define LN length()
+#define FST first
+#define SND second
+#define PB push_back
+#define PF push_front
+#define B begin()
+#define E end()
+#define nn '\n'
+#define ss ' '
+
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+
+}
+
+int32_t main()
+{
+	int t;
+
+	//cin >> t;
+	//t = 1;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
