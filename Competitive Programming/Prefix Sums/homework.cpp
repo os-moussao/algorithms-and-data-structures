638274c11ce8397718d91e3afb831240d3f1a340
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-02 23:55:29 +0100

    started pr3

---
+++ Competitive Programming/Prefix Sums/homework.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define int long long
+#define double long double
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+#define REP(i, a, n) for(int i = a; i < n; i++)
+#define SZ size()
+#define LN length()
+#define F first
+#define S second
+#define PB push_back
+#define PF push_front
+#define B begin()
+#define E end()
+#define nn '\n'
+#define ss ' '
+
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+	// todo
+}
+
+int32_t main()
+{
+	int t;
+
+	FILE *in = freopen("homework.in", "r", stdin);
+	FILE *out = freopen("homework.out", "w", stdout);
+	//cin >> t;
+	t = 1;
+	while (t--) {
+		solve();
+	}
+
+	fclose(in);
+	fclose(out);
+
+	return 0;
+}
