aa9bce48539c8a07d2afddcf3097da5b55a49dbc
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-03 23:13:42 +0100

    solved homework

--- Competitive Programming/Prefix Sums/homework.cpp
+++ Competitive Programming/Prefix Sums/homework.cpp
@@ -26,11 +26,40 @@ typedef vector<pi> vpi;
 #define nn '\n'
 #define ss ' '
 
-const int MOD = 1e9 + 7;
-
 void solve()
 {
-	// todo
+	int n;
+	cin >> n;
+
+	vi arr(n);
+	REP(i, 0, n) cin >> arr[i];
+
+	vi ans(0);
+
+	int div = 1;
+	int mini = min(arr[n - 1], arr[n - 2]);
+	int sum = arr[n - 1] + arr[n - 2];
+	double maxAvg = (double)(sum - mini) / (double) div;
+
+	ans.PB(n - 2);
+	for (int i = n - 3; i >= 1; i--)
+	{
+		mini = min(mini, arr[i]);
+		sum += arr[i];
+		div++;
+		double avg = (double)(sum - mini) / (double)div;
+		if (avg == maxAvg)
+			ans.PB(i);
+		else if (avg > maxAvg)
+		{
+			maxAvg = avg;
+			ans.clear();
+			ans.PB(i);
+		}
+	}
+
+	for (int i = ans.SZ - 1; i >= 0; i--)
+		cout << ans[i] << nn;
 }
 
 int32_t main()
@@ -39,7 +68,6 @@ int32_t main()
 
 	FILE *in = freopen("homework.in", "r", stdin);
 	FILE *out = freopen("homework.out", "w", stdout);
-	//cin >> t;
 	t = 1;
 	while (t--) {
 		solve();
