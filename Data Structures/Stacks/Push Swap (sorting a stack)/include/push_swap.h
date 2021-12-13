7decb20906fdbcd3182b123093336a70e6e35e1b
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-13 23:26:38 +0100

    added more prototypes

--- Data Structures/Stacks/Push Swap (sorting a stack)/include/push_swap.h
+++ Data Structures/Stacks/Push Swap (sorting a stack)/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 21:32:25 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/13 22:25:49 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/13 23:24:09 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,14 @@ typedef struct s_node
 	int				data;
 	struct s_node	*next;
 }				t_node;
+
+void	ft_swap(int *a, int *b);
+
+void	push(t_node **stack, int data);
+int		pop(t_node **stack);
+void	swap(t_node *stack);
+void	clear(t_node **stack);
+void	rotate(t_node *stack);
+void	rrotate(t_node *stack);
 
 #endif
