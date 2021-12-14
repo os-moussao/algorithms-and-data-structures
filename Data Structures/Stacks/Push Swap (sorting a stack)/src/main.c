c9a7bcfa3258de42e6e40f146c0ff73fe7c14890
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-14 22:01:49 +0100

    updated main

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/main.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/main.c
@@ -6,44 +6,17 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 22:09:37 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/14 02:05:30 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/14 21:59:52 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	ft_putnbr(int n)
-{
-	char	c;
-
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n *= -1;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	c = n % 10 + '0';
-	write(1, &c, 1);
-}
-
-void	disp(t_node *stack, char name)
-{
-	while (stack)
-	{
-		ft_putnbr(stack->data);
-		write(1, "\n", 1);
-		stack = stack->next;
-	}
-	write(1, "-\n", 2);
-	write(1, &name, 1);
-	write(1, "\n", 1);
-}
 
 int	main(int ac, char **av)
 {
 	t_node	*a;
 
 	a = create(av + 1, ac - 1);
+	//sort_stack(&a);
 	disp(a, 'a');
 }
