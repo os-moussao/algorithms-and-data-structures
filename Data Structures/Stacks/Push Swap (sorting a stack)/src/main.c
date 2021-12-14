eea29d189b049ea05b1ba2af73c4389bdbe2feae
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-14 02:01:27 +0100

    added display (applying the logic)

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/main.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/main.c
@@ -6,14 +6,44 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 22:09:37 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/13 22:14:27 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/14 01:50:52 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(void)
+void	ft_putnbr(int n)
 {
-	write(1, "Cool!\n", 6);
-	exit(0);
+	char	c;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n *= -1;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+void	disp(t_node *stack, char name)
+{
+	while (stack)
+	{
+		ft_putnbr(stack->data);
+		write(1, "\n", 1);
+		stack = stack->next;
+	}
+	write(1, "-\n", 2);
+	write(1, &name, 1);
+	write(1, "\n", 1);
+}
+
+int main(int ac, char **av)
+{
+	t_node	*a;
+
+	a = create(av + 1, ac - 1);
+	disp(a, 'a');
 }
