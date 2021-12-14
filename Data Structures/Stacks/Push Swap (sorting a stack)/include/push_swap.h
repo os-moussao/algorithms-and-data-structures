01ce1f9e5bd2ec857733877b0198a784e3d2bb1d
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-14 02:02:41 +0100

    added new prorotypes and macros

--- Data Structures/Stacks/Push Swap (sorting a stack)/include/push_swap.h
+++ Data Structures/Stacks/Push Swap (sorting a stack)/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 21:32:25 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/13 23:24:09 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/14 01:50:32 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 # include <unistd.h>
 # include <stdbool.h>
 
+# define I_MAX 2147483647
+# define I_MIN -2147483648
+
 typedef struct s_node
 {
 	int				data;
@@ -24,12 +27,15 @@ typedef struct s_node
 }				t_node;
 
 void	ft_swap(int *a, int *b);
+void	error(void);
 
+t_node	*create(char **av, int ac);
 void	push(t_node **stack, int data);
 int		pop(t_node **stack);
 void	swap(t_node *stack);
 void	clear(t_node **stack);
 void	rotate(t_node *stack);
 void	rrotate(t_node *stack);
+int		top(t_node *stack);
 
 #endif
