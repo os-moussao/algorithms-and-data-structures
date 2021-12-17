5da984194c32cac8e2c63995cad89ec416bf99d2
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-17 17:22:59 +0100

    updated sort.c

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 21:53:20 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/16 00:13:15 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/16 19:09:56 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,20 @@ static bool	is_sorted(t_node *stack)
 	stack = stack->next;
 	while (stack)
 	{
-		if (stack->data > x)
+		if (stack->data < x)
 			return (false);
+		x = stack->data;
+		stack = stack->next;
 	}
 	return (true);
 }
 
 t_node	*sort_stack(t_node *a)
 {
-	//return (a);
-	
-	//t_node	*b;
-
+	t_node	*b;
 
 	if (is_sorted(a))
 		return (a);
-	return (a);
-
-	/*
 	if (!(a->next->next))
 	{
 		if (a->data > a->next->data)
@@ -47,28 +43,27 @@ t_node	*sort_stack(t_node *a)
 			ft_putstr("sa\n");
 			swap(a);
 		}
-		return ;
+		return (a);
 	}
 	b = NULL;
 	ft_putstr("pb\n");
-	push(&b, pop(a));
-	while (*a)
+	push(&b, pop(&a));
+	while (a)
 	{
-		//if ((*a)->data > b->data)
-		//{
-			ft_putstr("pb\n");
-			push(&b, pop(a));
-		//}
-		//else
-		//{
-		if (b->data < b->next->data)
+		ft_putstr("pb\n");
+		push(&b, pop(&a));
+		while (b->next && b->data < b->next->data)
 		{
 			ft_putstr("sb\n");
 			swap(b);
 			ft_putstr("pa\n");
-			push(a, pop(&b));
+			push(&a, pop(&b));
 		}
-		//}
 	}
-	*/
+	while (b)
+	{
+		ft_putstr("pa\n");
+		push(&a, pop(&b));
+	}
+	return (a);
 }
