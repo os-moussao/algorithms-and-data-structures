f43e4f1a26f75e501f0ef2ec4fb159febb2042ba
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-18 01:56:32 +0100

    added algo for three elements

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 21:53:20 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/16 19:09:56 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/18 01:54:39 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,21 +30,65 @@ static bool	is_sorted(t_node *stack)
 	return (true);
 }
 
-t_node	*sort_stack(t_node *a)
+// you may not apply the instructions if you want
+t_node	*sort_three(t_node *a)
 {
-	t_node	*b;
-
-	if (is_sorted(a))
-		return (a);
-	if (!(a->next->next))
+	if (a->data < a->next->data)
 	{
+		ft_putstr("rra\n");
+		rrotate(a);
 		if (a->data > a->next->data)
 		{
 			ft_putstr("sa\n");
-			swap(a);
+			//swap(a);
 		}
+	}
+	else
+	{
+		if (a->data > a->next->data && a->next->data > bottom(a))
+		{
+			ft_putstr("sa\n");
+			//swap(a);
+			ft_putstr("rra\n");
+			//rrotate(a);
+		}
+		else if (a->data > bottom(a))
+		{
+			ft_putstr("ra\n");
+			//rotate(a);
+		}
+		else
+		{
+			ft_putstr("sa\n");
+			//swap(a);
+		}
+	}
+	return (a);
+}
+
+t_node	*sort_stack(t_node *a)
+{
+	t_node	*b;
+	int		len;
+
+	// if sorted quit
+	if (is_sorted(a))
+		return (a);
+
+	// calc stack len
+	len = stack_len(a);
+
+	// in case if two or three
+	if (len == 2)
+	{
+		if (a->data > a->next->data)
+			ft_putstr("sa\n");
 		return (a);
 	}
+	else if (len == 3)
+		return (sort_three(a));
+
+	// dumb algorithm for the remaining lengths
 	b = NULL;
 	ft_putstr("pb\n");
 	push(&b, pop(&a));
