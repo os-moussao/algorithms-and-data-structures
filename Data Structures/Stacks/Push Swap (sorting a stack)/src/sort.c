fb38f82a7a3a467e368a622da49bb2435c76fc02
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-20 22:17:01 +0100

    added sorting algo for five elements

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 21:53:20 by omoussao          #+#    #+#             */
-/*   Updated: 2021/12/18 01:54:39 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/12/20 21:04:56 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,30 +40,105 @@ t_node	*sort_three(t_node *a)
 		if (a->data > a->next->data)
 		{
 			ft_putstr("sa\n");
-			//swap(a);
+			swap(a);
 		}
 	}
 	else
 	{
 		if (a->data > a->next->data && a->next->data > bottom(a))
 		{
-			ft_putstr("sa\n");
-			//swap(a);
-			ft_putstr("rra\n");
-			//rrotate(a);
+			ft_putstr("sa\nrra\n");
+			swap(a);
+			rrotate(a);
 		}
 		else if (a->data > bottom(a))
 		{
 			ft_putstr("ra\n");
-			//rotate(a);
+			rotate(a);
 		}
 		else
 		{
 			ft_putstr("sa\n");
-			//swap(a);
+			swap(a);
 		}
 	}
 	return (a);
+}
+
+t_node	*insert_five_one(t_node *a, t_node *b)
+{
+	int		tmp;
+	bool	rrb;
+
+	if (b->data > bottom(a))
+	{
+		ft_putstr("pa\nra\n");
+		push(&a, pop(&b));
+		rotate(a);
+	}
+	else if (b->data > a->next->next->next->data)
+	{
+		ft_putstr("pa\nrra\nsa\nra\nra\n");
+		push(&a, pop(&b));
+		rrotate(a);
+		swap(a);
+		rotate(a);
+		rotate(a);
+	}
+	else
+	{
+		tmp = b->data;
+		rrb = tmp > a->data;
+		while (a->data < tmp)
+		{
+			ft_putstr("pb\n");
+			push(&b, pop(&a));
+		}
+		if (rrb)
+		{
+			ft_putstr("rrb\n");
+			rrotate(b);
+		}
+		while (b)
+		{
+			ft_putstr("pa\n");
+			push(&a, pop(&b));
+		}
+	}
+	return (a);
+}
+
+t_node	*sort_five(t_node *a)
+{
+	t_node	*b;
+
+	b = NULL;
+	ft_putstr("pb\npb\n");
+	push(&b, pop(&a));
+	push(&b, pop(&a));
+	if (!is_sorted(a))
+		a = sort_three(a);
+	ft_putstr("pa\n");
+	push(&a, pop(&b));
+	if (a->data > bottom(a))
+	{
+		ft_putstr("ra\n");
+		rotate(a);
+	}
+	else if (a->data > a->next->next->data)
+	{
+		ft_putstr("rra\nsa\nra\nra\n");
+		rrotate(a);
+		swap(a);
+		rotate(a);
+		rotate(a);
+	}
+	else if (a->data > a->next->data)
+	{
+		ft_putstr("sa\n");
+		swap(a);
+	}
+	return (insert_five_one(a, b));
 }
 
 t_node	*sort_stack(t_node *a)
@@ -87,6 +162,8 @@ t_node	*sort_stack(t_node *a)
 	}
 	else if (len == 3)
 		return (sort_three(a));
+	else if (len == 5)
+		return (sort_five(a));
 
 	// dumb algorithm for the remaining lengths
 	b = NULL;
