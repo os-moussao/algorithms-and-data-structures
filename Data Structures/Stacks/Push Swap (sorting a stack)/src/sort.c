c643b644d9842e0caf2c7e9f162eb15e3b4fd6b9
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-21 00:35:43 +0100

    solved algo for 4 and 5 values

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
@@ -30,6 +30,27 @@ static bool	is_sorted(t_node *stack)
 	return (true);
 }
 
+static int find_min(t_node *a)
+{
+	int	min;
+	int	min_i;
+	int	i;
+
+	i = 0;
+	min = I_MAX;
+	while (a)
+	{
+		if (a->data <= min)
+		{
+			min = a->data;
+			min_i = i;
+		}
+		i++;
+		a = a->next;
+	}
+	return (min_i);
+}
+
 // you may not apply the instructions if you want
 t_node	*sort_three(t_node *a)
 {
@@ -65,56 +86,39 @@ t_node	*sort_three(t_node *a)
 	return (a);
 }
 
-t_node	*insert_five_one(t_node *a, t_node *b)
-{
-	int		tmp;
-	bool	rrb;
-
-	if (b->data > bottom(a))
-	{
-		ft_putstr("pa\nra\n");
-		push(&a, pop(&b));
-		rotate(a);
-	}
-	else if (b->data > a->next->next->next->data)
-	{
-		ft_putstr("pa\nrra\nsa\nra\nra\n");
-		push(&a, pop(&b));
-		rrotate(a);
-		swap(a);
-		rotate(a);
-		rotate(a);
-	}
-	else
-	{
-		tmp = b->data;
-		rrb = tmp > a->data;
-		while (a->data < tmp)
-		{
-			ft_putstr("pb\n");
-			push(&b, pop(&a));
-		}
-		if (rrb)
-		{
-			ft_putstr("rrb\n");
-			rrotate(b);
-		}
-		while (b)
-		{
-			ft_putstr("pa\n");
-			push(&a, pop(&b));
-		}
-	}
-	return (a);
-}
-
-t_node	*sort_five(t_node *a)
+t_node	*sort_foor(t_node *a)
 {
 	t_node	*b;
+	int		min;
 
 	b = NULL;
-	ft_putstr("pb\npb\n");
+	min = find_min(a);
+	if (min == 1)
+	{
+		ft_putstr("sa\n");
+		swap(a);
+	}
+	else if (min == 2)
+	{
+		ft_putstr("rra\nrra\n");
+		rrotate(a);
+		rrotate(a);
+	}
+	else if (min == 3)
+	{
+		ft_putstr("rra\n");
+		rrotate(a);
+	}
+	ft_putstr("pb\n");
 	push(&b, pop(&a));
+	if (!is_sorted(a))
+		a = sort_three(a);
+	ft_putstr("pa\n");
+	push(&a, pop(&b));
+	return (a);
+	/*
+	b = NULL;
+	ft_putstr("pb\n");
 	push(&b, pop(&a));
 	if (!is_sorted(a))
 		a = sort_three(a);
@@ -137,8 +141,51 @@ t_node	*sort_five(t_node *a)
 	{
 		ft_putstr("sa\n");
 		swap(a);
+	}*/
+}
+
+t_node	*sort_five(t_node *a)
+{
+	t_node	*b;
+	int		min;
+
+	b = NULL;
+	min = find_min(a);
+	if (min == 1)
+	{
+		ft_putstr("sa\n");
+		swap(a);
 	}
-	return (insert_five_one(a, b));
+	else if (min == 2)
+	{
+		ft_putstr("ra\nra\n");
+		rotate(a);
+		rotate(a);
+	}
+	else if (min == 3)
+	{
+		ft_putstr("rra\nrra\n");
+		rrotate(a);
+		rrotate(a);
+	}
+	else if (min == 4)
+	{
+		ft_putstr("rra\n");
+		rrotate(a);
+	}
+	ft_putstr("pb\n");
+	push(&b, pop(&a));
+	if (!is_sorted(a))
+		a = sort_foor(a);
+	ft_putstr("pa\n");
+	push(&a, pop(&b));
+	return (a);
+	/*
+	ft_putstr("pb\n");
+	push(&b, pop(&a));
+	if (!is_sorted(a))
+		a = sort_foor(a);
+	return (insert_five_one(a, b));*/
 }
 
 t_node	*sort_stack(t_node *a)
@@ -162,6 +209,8 @@ t_node	*sort_stack(t_node *a)
 	}
 	else if (len == 3)
 		return (sort_three(a));
+	else if (len == 4)
+		return (sort_foor(a));
 	else if (len == 5)
 		return (sort_five(a));
 
@@ -188,3 +237,58 @@ t_node	*sort_stack(t_node *a)
 	}
 	return (a);
 }
+
+
+
+
+
+
+t_node	*insert_five_one(t_node *a, t_node *b)
+{
+	int		tmp;
+	bool	rrb;
+
+	if (b->data > bottom(a))
+	{
+		ft_putstr("pa\nra\n");
+		push(&a, pop(&b));
+		rotate(a);
+	}
+	else if (b->data > a->next->next->data)
+	{
+		ft_putstr("pa\nrra\nsa\nra\nra\n");
+		push(&a, pop(&b));
+		rrotate(a);
+		swap(a);
+		rotate(a);
+		rotate(a);
+	}
+	else if (b->data > a->data && b->data < a->next->data)
+	{
+		ft_putstr("pa\nsa\n");
+		push(&a, pop(&b));
+		swap(a);
+	}
+	else
+	{
+		tmp = b->data;
+		rrb = tmp > a->data;
+		while (a->data < tmp)
+		{
+			ft_putstr("pb\n");
+			push(&b, pop(&a));
+		}
+		if (rrb)
+		{
+			ft_putstr("rrb\n");
+			rrotate(b);
+		}
+		while (b)
+		{
+			ft_putstr("pa\n");
+			push(&a, pop(&b));
+		}
+	}
+	return (a);
+}
+
