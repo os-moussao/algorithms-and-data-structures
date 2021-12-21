93b9bfb46beb43e067cbb0c9f302fd9dbe93c230
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-21 17:54:08 +0100

    added selection sort

--- Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
+++ Data Structures/Stacks/Push Swap (sorting a stack)/src/sort.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-static bool	is_sorted(t_node *stack)
+static bool	sorted(t_node *stack)
 {
 	int	x;
 
@@ -51,7 +51,31 @@ static int find_min(t_node *a)
 	return (min_i);
 }
 
-// you may not apply the instructions if you want
+t_node	*min_up(t_node *a, int len)
+{
+	int	min;
+
+	min = find_min(a);
+	if (min < len / 2 + (len & 1))
+	{
+		while (min--)
+		{
+			ft_putstr("ra\n");
+			rotate(a);
+		}
+	}
+	else
+	{
+		min = len - min;
+		while (min--)
+		{
+			ft_putstr("rra\n");
+			rrotate(a);
+		}
+	}
+	return (a);
+}
+
 t_node	*sort_three(t_node *a)
 {
 	if (a->data < a->next->data)
@@ -86,106 +110,25 @@ t_node	*sort_three(t_node *a)
 	return (a);
 }
 
-t_node	*sort_foor(t_node *a)
+t_node	*selection_sort(t_node *a, int len)
 {
 	t_node	*b;
-	int		min;
 
 	b = NULL;
-	min = find_min(a);
-	if (min == 1)
+	while (len > 3)
 	{
-		ft_putstr("sa\n");
-		swap(a);
+		a = min_up(a, len--);
+		ft_putstr("pb\n");
+		push(&b, pop(&a));
 	}
-	else if (min == 2)
-	{
-		ft_putstr("rra\nrra\n");
-		rrotate(a);
-		rrotate(a);
-	}
-	else if (min == 3)
-	{
-		ft_putstr("rra\n");
-		rrotate(a);
-	}
-	ft_putstr("pb\n");
-	push(&b, pop(&a));
-	if (!is_sorted(a))
+	if (!sorted(a))
 		a = sort_three(a);
-	ft_putstr("pa\n");
-	push(&a, pop(&b));
+	while (b)
+	{
+		ft_putstr("pa\n");
+		push(&a, pop(&b));
+	}
 	return (a);
-	/*
-	b = NULL;
-	ft_putstr("pb\n");
-	push(&b, pop(&a));
-	if (!is_sorted(a))
-		a = sort_three(a);
-	ft_putstr("pa\n");
-	push(&a, pop(&b));
-	if (a->data > bottom(a))
-	{
-		ft_putstr("ra\n");
-		rotate(a);
-	}
-	else if (a->data > a->next->next->data)
-	{
-		ft_putstr("rra\nsa\nra\nra\n");
-		rrotate(a);
-		swap(a);
-		rotate(a);
-		rotate(a);
-	}
-	else if (a->data > a->next->data)
-	{
-		ft_putstr("sa\n");
-		swap(a);
-	}*/
-}
-
-t_node	*sort_five(t_node *a)
-{
-	t_node	*b;
-	int		min;
-
-	b = NULL;
-	min = find_min(a);
-	if (min == 1)
-	{
-		ft_putstr("sa\n");
-		swap(a);
-	}
-	else if (min == 2)
-	{
-		ft_putstr("ra\nra\n");
-		rotate(a);
-		rotate(a);
-	}
-	else if (min == 3)
-	{
-		ft_putstr("rra\nrra\n");
-		rrotate(a);
-		rrotate(a);
-	}
-	else if (min == 4)
-	{
-		ft_putstr("rra\n");
-		rrotate(a);
-	}
-	ft_putstr("pb\n");
-	push(&b, pop(&a));
-	if (!is_sorted(a))
-		a = sort_foor(a);
-	ft_putstr("pa\n");
-	push(&a, pop(&b));
-	return (a);
-	/*
-	ft_putstr("pb\n");
-	push(&b, pop(&a));
-	if (!is_sorted(a))
-		a = sort_foor(a);
-	return (insert_five_one(a, b));*/
 }
 
 t_node	*sort_stack(t_node *a)
@@ -194,7 +137,7 @@ t_node	*sort_stack(t_node *a)
 	int		len;
 
 	// if sorted quit
-	if (is_sorted(a))
+	if (sorted(a))
 		return (a);
 
 	// calc stack len
@@ -209,10 +152,14 @@ t_node	*sort_stack(t_node *a)
 	}
 	else if (len == 3)
 		return (sort_three(a));
+	return (selection_sort(a, len));
+	/*
 	else if (len == 4)
 		return (sort_foor(a));
 	else if (len == 5)
 		return (sort_five(a));
+	else // if (len < 100)
+		return (selection_sort(a, len));*/
 
 	// dumb algorithm for the remaining lengths
 	b = NULL;
@@ -237,58 +184,3 @@ t_node	*sort_stack(t_node *a)
 	}
 	return (a);
 }
-
-
-
-
-
-
-t_node	*insert_five_one(t_node *a, t_node *b)
-{
-	int		tmp;
-	bool	rrb;
-
-	if (b->data > bottom(a))
-	{
-		ft_putstr("pa\nra\n");
-		push(&a, pop(&b));
-		rotate(a);
-	}
-	else if (b->data > a->next->next->data)
-	{
-		ft_putstr("pa\nrra\nsa\nra\nra\n");
-		push(&a, pop(&b));
-		rrotate(a);
-		swap(a);
-		rotate(a);
-		rotate(a);
-	}
-	else if (b->data > a->data && b->data < a->next->data)
-	{
-		ft_putstr("pa\nsa\n");
-		push(&a, pop(&b));
-		swap(a);
-	}
-	else
-	{
-		tmp = b->data;
-		rrb = tmp > a->data;
-		while (a->data < tmp)
-		{
-			ft_putstr("pb\n");
-			push(&b, pop(&a));
-		}
-		if (rrb)
-		{
-			ft_putstr("rrb\n");
-			rrotate(b);
-		}
-		while (b)
-		{
-			ft_putstr("pa\n");
-			push(&a, pop(&b));
-		}
-	}
-	return (a);
-}
-
