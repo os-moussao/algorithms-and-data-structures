48adf96c8f58ef1d6d05643eeb8ed6ee6a266346
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-12-22 18:43:08 +0100

    added more problems

---
+++ Competitive Programming/Practice/Training/Section A/Elementary Math/04_juicer.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define int long long
+#define double long double
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<char, int> pci;
+typedef vector<pii> vpii;
+typedef vector<pdd> vpdd;
+typedef vector<pci> vpci;
+
+#define REP(i, a, n) for(int i = a; i < n; i++)
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define RREP(i, n, a) for(int i = n; i >= a; i--)
+#define RFOR(i, n) for(int i = n; i >= 0; i--)
+#define ITER(i, a, b) for(auto i = a; i != b; i++)
+#define SZ size()
+#define L length()
+#define F first
+#define S second
+#define PB push_back
+#define PF push_front
+#define B begin()
+#define E end()
+#define nn '\n'
+#define ss ' '
+
+void solve()
+{
+	int n, b, d;
+	cin >> n >> b >> d;
+	int ans = 0;
+	int x = 0;
+	FOR(i, n)
+	{
+		int o;
+		cin >> o;
+		if (o <= b)
+			x += o;
+		if (x > d)
+		{
+			ans++;
+			x = 0;
+		}
+	}
+	cout << ans << nn;
+}
+
+int32_t main()
+{
+	int t;
+
+	//cin >> t;
+	t = 1;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
