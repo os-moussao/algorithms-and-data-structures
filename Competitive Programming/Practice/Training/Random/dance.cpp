e1848f2e0fa8cb32936ad80a5b76aad6a1f58850
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-06 13:54:47 +0100

    added more problems

---
+++ Competitive Programming/Practice/Training/Random/dance.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define int long long
+#define double long double
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<char, int> pci;
+typedef vector<pii> vpii;
+typedef vector<pdd> vpdd;
+typedef vector<pci> vpci;
+
+#define REP(i, a, n) for(int i = a; i < n; i++)
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define RREP(i, n, a) for(int i = n; i >= a; i--)
+#define RFOR(i, n) for(int i = n; i >= 0; i--)
+#define ITER(i, a, b) for(auto i = a; i != b; i++)
+#define SZ size()
+#define L length()
+#define F first
+#define S second
+#define PB push_back
+#define PF push_front
+#define B begin()
+#define E end()
+#define nn '\n'
+#define ss ' '
+
+void solve()
+{
+	int N;
+	cin >> N;
+	int NN = 2 * N * N - N;
+	vvi a(2 * N - 1);
+
+	FOR(i, 2 * N - 1) {
+		int k = 2 * N - 1 - i;
+		FOR(i, k) {
+			int x; cin >> x;
+			a[i].PB(x);
+		}
+	}
+
+
+	FOR(i, 2 * N - 1) {
+		int k = 2 * N - 1 - i;
+		FOR(i, k) {
+			cout << a[i][k] << ss;
+		}
+		cout << nn;
+	}
+}
+
+int32_t main()
+{
+	int t;
+
+	//cin >> t;
+	t = 1;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
