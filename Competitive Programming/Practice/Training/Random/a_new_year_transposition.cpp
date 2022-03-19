4cba7ff31b16892ae3674673db0077fea482bef6
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-03-19 15:31:08 +0100

    add 2 problems

---
+++ Competitive Programming/Practice/Training/Random/a_new_year_transposition.cpp
@@ -0,0 +1,81 @@
+// https://codeforces.com/problemset/problem/500/A
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+#define int long long
+#define double long double
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef pair<double, double> pdd;
+typedef pair<char, int> pci;
+typedef vector<pci> vpci;
+
+#define REP(i, a, n) for(int i = a; i < n; i++)
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define RREP(i, n, a) for(int i = n; i >= a; i--)
+#define RFOR(i, n) for(int i = n; i >= 0; i--)
+#define ITER(i, a, b) for(auto i = a; i != b; i++)
+#define SZ size()
+#define L length()
+#define F first
+#define S second
+#define MP(x, y) make_pair(x, y)
+#define PB push_back
+#define PF push_front
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define ALL(a) a.B, a.E
+#define RALL(a) a.RB, a.RE
+#define nn '\n'
+#define ss ' '
+#define YESORNO(x) cout << (x? "YES\n": "NO\n")
+
+void solve()
+{
+	int n, target; cin >> n >> target;
+	bool ans = 0;
+	vi arr(n - 1);
+	FOR (i, n - 1) {
+		cin >> arr[i];
+	}
+	int i = 0;
+	while (i < n - 1) {
+		if (i == target - 1) {
+			ans = 1;
+			break ;
+		}
+		i += arr[i];
+	}
+	if (i == target - 1) ans = 1;
+	YESORNO(ans);
+}
+
+int32_t main()
+{
+	int t;
+
+	//cin >> t;
+	t = 1;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
