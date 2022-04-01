2d197a8e0db10e337867774c6eb6ca9cd2d36991
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-04-01 18:46:21 +0000

    Add Codeforces Round #780 (Div. 3)

---
+++ Competitive Programming/Codeforces/div. 3/Codeforces Round #780 (Div. 3)/B_Vlad_and_Candies.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+#define int long long
+#define double long double
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef pair<double, double> pdd;
+typedef pair<char, int> pci;
+typedef vector<pci> vpci;
+
+#define REP(i, a, n) for(int i = a; i < n; i++)
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define RREP(i, n, a) for(int i = n; i >= a; i--)
+#define RFOR(i, n) for(int i = n; i >= 0; i--)
+#define ITER(i, a, b) for(auto i = a; i != b; i++)
+#define SZ size()
+#define L length()
+#define F first
+#define S second
+#define MP(x, y) make_pair(x, y)
+#define PB push_back
+#define PF push_front
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define ALL(a) a.B, a.E
+#define RALL(a) a.RB, a.RE
+#define nn '\n'
+#define ss ' '
+#define YESORNO(x) cout << (x? "YES\n": "NO\n")
+#define MAXVEC(vec) *max_element(ALL(vec))
+#define MINVEC(vec) *min_element(ALL(vec))
+#define getunique(vec)  {sort(vec.begin(), vec.end()); vec.erase(unique(vec.begin(), vec.end()), vec.end());}
+
+void solve()
+{
+	int n;
+	cin >> n;
+	vi arr(n);
+	FOR (i, n) cin >> arr[i];
+	if (arr.SZ == 1) {
+		YESORNO(arr[0] <= 1);
+		return ;
+	}
+	int i = n - 1;
+	sort(ALL(arr));
+	YESORNO(arr[i] - arr[i - 1] <= 1);
+	// while (i > 0) {
+	// 	if (arr[i] - arr[i - 1] >= 2) {
+	// 		cout << "NO\n";
+	// 		return ;
+	// 	}
+	// 	else {
+	// 		i -= 2;
+	// 	}
+	// }
+	// cout << "YES\n";
+}
+
+int32_t main()
+{
+	int t;
+
+	cin >> t;
+	//t = 1;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
