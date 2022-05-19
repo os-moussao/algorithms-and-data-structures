5cb49d92b7a0cb707661e2e359644d66f12f0540
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-05-19 13:07:08 +0100

    Update SegTree section
    Solved:
    https://cses.fi/problemset/task/1647
    https://cses.fi/problemset/task/1650

---
+++ Data Structures/Segment trees/RangeXor.cpp
@@ -0,0 +1,45 @@
+// link: https://cses.fi/problemset/task/1650
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+#define endl '\n'
+
+struct segTree {
+	typedef int T;
+	static const T identity = 0;
+	T fn(T a, T b) { return a ^ b; } // (any associative fn)
+	vector<T> s; int n;
+	segTree(int n = 0, T def = identity) : s(4*n, def), n(n) {}
+	void update(int pos, T val) {
+		for (s[pos += n] = val; pos /= 2;)
+			s[pos] = fn(s[pos * 2], s[pos * 2 + 1]);
+	}
+	T query(int b, int e) { // query [b, e)
+		T ra = identity, rb = identity;
+		for (b += n, e += n; b < e; b /= 2, e /= 2) {
+			if (b % 2) ra = fn(ra, s[b++]);
+			if (e % 2) rb = fn(s[--e], rb);
+		}
+		return fn(ra, rb);
+	}
+};
+
+// Range Xor Queries
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	segTree st(n);
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		st.update(i, x);
+	}
+	while (m--) {
+		int a, b;
+		cin >> a >> b;
+		--a;
+		cout << st.query(a, b) << endl;
+	}
+}
