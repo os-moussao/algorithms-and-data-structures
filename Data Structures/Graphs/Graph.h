807fbc93ccc27065474b687a56e206131a9111d7
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-05-26 15:52:48 +0100

    Add Graphs template

---
+++ Data Structures/Graphs/Graph.h
@@ -0,0 +1,136 @@
+#pragma once
+
+class Graph {
+	int V;
+public:
+	vector<vector<int>> adj;
+	Graph(): V(0) {};
+	Graph(int n) {V = n; adj.resize(n);}
+	void edge(int u, int v) {adj[u].push_back(v);}
+	void uedge(int u, int v) {edge(u, v); edge(v,u);}
+	int  size() {return V;}
+	// BFS: shortest path from s to e
+	vector <int> BFS(int s, int e) {
+		int par[V]; par[s]= -1;
+		bool vis[V]; memset(vis, 0, sizeof(vis));
+		queue <int> Q; Q.push(s);
+		vis[s] = true;
+		while (!Q.empty()) {
+			int v = Q.front(); Q.pop();
+			if (v == e) {
+				vector <int> _path;
+				while (v != -1) {
+					_path.push_back(v); // just v if zero indexed
+					v = par[v];
+				}
+				return (reverse(_path.begin(), _path.end()), _path);
+			}
+			for (int u: adj[v]) {
+				if (!vis[u]) {
+					par[u] = v;
+					Q.push(u);
+					vis[u] = true;
+				}
+			}
+		}
+		return {};
+	}
+	// Cycle detection
+	enum state {UNVIS,IN_STACK,VIS};
+	bool dcycle(void) {
+		vector<state> vis(V, UNVIS);
+		for (int i = 0; i < V; i++)
+			if (vis[i] == UNVIS && dfs_dcycle(i, vis)) return true;
+		return false;
+	}
+	bool ucycle(void) {
+		vector<bool> vis(V, 0);
+		for (int i = 0; i < V; i++)
+			if (!vis[i] && dfs_ucycle(i, vis, -1)) return true;
+		return false;
+	}
+	stack <int> TP_sort() {
+		bool vis[V]; memset(vis, 0, sizeof(vis));
+		stack <int> S;
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) TP_dfs_helper(i, vis, S);
+		}
+		return S;
+	}
+	// connected components
+	int ccnt() {
+		int cnt = 0;
+		bool vis[V]; memset(vis, 0, sizeof(vis));
+		for (int v = 0; v < V; v++) {
+			if (!vis[v]) { cnt++; DFS(v, vis); }
+		}
+		return cnt;
+	}
+	// debug graph
+	void print(void) {
+		for (int i = 0; i < V; i++) {
+			cerr << i << ": ";
+			for (int u: adj[i]) cerr << u << ' ';
+			cerr << '\n';
+		}
+	}
+private:
+	void DFS(int s, bool vis[]) {
+		vis[s] = true;
+		for (int u: adj[s]) {
+			if (!vis[u]) {
+				DFS(u, vis);
+			}
+		}
+	}
+	bool dfs_dcycle(int s, vector<state> &vis)
+	{
+		vis[s] = IN_STACK;
+		for (int u: adj[s]) {
+			if (vis[u] == IN_STACK) return true;
+			if (vis[u] == UNVIS && dfs_dcycle(u, vis)) return true;
+		}
+		vis[s] = VIS;
+		return false;
+	}
+	bool dfs_ucycle(int s, vector<bool> &vis, int par) {
+		vis[s] = 1;
+		for (int u: adj[s]) {
+			if (vis[u] && u != par) return true;
+			if (!vis[u] && dfs_ucycle(u, vis, s)) return true;
+		}
+		return false;
+	}
+	void TP_dfs_helper(int s, bool vis[], stack<int> &S) {
+		vis[s] = true;
+		for (int u: adj[s]) {
+			if (!vis[u]) TP_dfs_helper(u, vis, S);
+		}
+		S.push(s);
+	}
+};
+
+// Longest path in a directed acyclic graph O(N+M)
+// src: geeks_for_geeks 
+void ldp_dfs(Graph &g, int node, int dp[], bool vis[])
+{
+    vis[node] = true;
+    for (int i = 0; i < g.adj[node].size(); i++) {
+        if (!vis[g.adj[node][i]])
+            ldp_dfs(g, g.adj[node][i], dp, vis);
+        dp[node] = max(dp[node], 1 + dp[g.adj[node][i]]);
+    }
+}
+int longest_directed_path(Graph &g)
+{
+    int ans = 0;
+	int n = g.size();
+    int dp[n]; memset(dp, 0, sizeof(dp));
+    bool vis[n]; memset(vis, 0, sizeof(vis));
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) ldp_dfs(g, i, dp, vis);
+    }
+    for (int i = 0; i < n; i++)
+        ans = max(ans, dp[i]);
+    return ans;
+}
