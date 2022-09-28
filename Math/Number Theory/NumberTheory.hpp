d6949f786d8c95d35d2f00a222fe249c550d6f66
Oussama Moussaoui <omoussaoui1337@gmail.com>
2022-09-29 00:04:48 +0100

    Add Number Theory template

---
+++ Math/Number Theory/NumberTheory.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+const int MOD = (1e9 + 7);
+
+int power(int a, int b, int c=MOD) {
+	int res = 1;
+	a %= MOD;
+	while (b) {
+		if (b&1) res = (res * a) % MOD;
+		a = (a * a) % MOD;
+		b >>= 1;
+	}
+	return res;
+}
+
+int inv(int a) {
+	return power(a, MOD-2);
+}
+
+const int MAXN = 2e5 + 5;
+int fact[MAXN], ifact[MAXN];
+void preFact() {
+	fact[0] = ifact[0] = 1;
+	for (int i = 1; i < MAXN; i++) {
+		fact[i] = (fact[i-1] * i) % MOD;
+		ifact[i] = (ifact[i-1] * inv(i)) % MOD;
+	}
+}
+
+int nCk(int n, int k) {
+	return k > n? 0: (fact[n] * ((ifact[n-k] * ifact[k]) % MOD)) % MOD;
+}
