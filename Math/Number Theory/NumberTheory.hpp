8bf44aa5a52fededa9aebb01308f9ce64c29db6b
Oussama Moussaoui <omoussaoui1337@gmail.com>
2022-10-01 16:39:23 +0100

    Update Number Theory template
    - Add prime factorization

--- Math/Number Theory/NumberTheory.hpp
+++ Math/Number Theory/NumberTheory.hpp
@@ -31,6 +31,20 @@ int nCk(int n, int k) {
 	return k > n? 0: (fact[n] * ((ifact[n-k] * ifact[k]) % MOD)) % MOD;
 }
 
+// Prime factorization
+vector<int> prime_fact(int n) {
+	vector<int> ret;
+	for (int i = 2; i*i <= n; i++) {
+		if (n%i == 0) {
+			ret.push_back(i);
+			while (n%i == 0)
+				n /= i;
+		}
+	}
+	if (n > 1) ret.push_back(n);
+	return ret;
+}
+
 // Euler's totient function
 // a^phi(m) = 1 [modulo m] if a and m are coprimes
 int phi(int x) {
