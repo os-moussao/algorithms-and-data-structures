ed8cb548539fc61028fbe64071a79399a722c13e
Oussama Moussaoui <omoussaoui1337@gmail.com>
2022-10-13 20:43:59 +0100

    Add Range Min-Max Queries

---
+++ Data Structures/RMQ/RMQ.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+// from kactl library: https://github.com/kth-competitive-programming/kactl/
+template<class T>
+struct RMQ {
+	int fn(T a, T b) {return max(a, b); }
+	vector<vector<T>> jmp;
+	RMQ(const vector<T>& V) : jmp(1, V) {
+		for (int pw = 1, k = 1; pw * 2 <= SZ(V); pw *= 2, ++k) {
+			jmp.emplace_back(SZ(V) - pw * 2 + 1);
+			rep(j,0,SZ(jmp[k]))
+				jmp[k][j] = fn(jmp[k - 1][j], jmp[k - 1][j + pw]);
+		}
+	}
+	T query(int a, int b) { b++; // [a,b]
+		assert(a < b); // or return inf if a == b
+		int dep = 31 - __builtin_clz(b - a);
+		return fn(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+	}
+};
