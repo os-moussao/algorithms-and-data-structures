87475d63877dd9d82ee08e2006094f95b415639e
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-07-27 03:34:47 +0100

    NumberTheory: add smallest prime factor, prime factorization

--- Math/Number Theory/Seive.hpp
+++ Math/Number Theory/Seive.hpp
@@ -15,3 +15,27 @@ void pre() {
 	    if (!seive[i]) primes.push_back(i);
 	}
 }
+
+// v[i] = smallest prime factor of i
+vector<int> v(1e7+1);
+void fun() {
+	for (int i = 0; i <= 1e7; i++) v[i] = i;
+	for (int i = 2; i <= 1e7; i++) {
+		if (v[i] != i) continue ;
+		for (int j = i*i; j <= 1e7; j += i) {
+			if (v[j]==j) v[j] = i;
+		}
+	}
+}
+
+// prime fact using smallest factor vector
+vector<int> prime_fact(int n) {
+	vector<int> fct;
+	while (n != 1) {
+		int i = v[n];
+		while (n % i == 0)
+			n /= i;
+		fct.push_back(i);
+	}
+	return fct;
+}
