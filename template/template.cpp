5a24eb73699d710d2a9914a93af4be551bfac5b6
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-10-24 16:13:37 +0100

    chore: add policy based tree

--- template/template.cpp
+++ template/template.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+template <class T> using OrderedSet = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template <class T> using MultiOrderedSet = tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 #define int long long
 #define double long double
 #define endl '\n'
