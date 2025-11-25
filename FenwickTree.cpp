4273525a48247d7ecc3a07d0829d386847ba9f7a
Oussama Moussaoui <omoussaoui1337@gmail.com>
2025-11-25 16:15:07 +0100

    add fenwich tree lower bound

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -31,3 +31,27 @@ public:
     return l == r ? a[l]: l > 0 ? invFn(query(r), query(l-1)): query(r);
   }
 };
+
+// Kactl
+struct FT {
+	vector<int> s;
+	FT(int n) : s(n) {}
+	void update(int pos, int dif) { // a[pos] += dif
+		for (; pos < s.size(); pos |= pos + 1) s[pos] += dif;
+	}
+	int query(int pos) { // sum of values in [0, pos)
+		int res = 0;
+		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
+		return res;
+	}
+	int lower_bound(int sum) {// min pos st sum of [0, pos] >= sum
+		// Returns n if no sum is >= sum, or -1 if empty sum is.
+		if (sum <= 0) return -1;
+		int pos = 0;
+		for (int pw = 1 << 25; pw; pw >>= 1) {
+			if (pos + pw <= s.size() && s[pos + pw-1] < sum)
+				pos += pw, sum -= s[pos-1];
+		}
+		return pos;
+	}
+};
